f5f9f95c2b4d3879cc3e1b11a32fbc628922c4c7
Daniel <mytechde@outlook.com>
2018-02-19 12:09:16 +0100

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,12 @@ import { HomePage } from '../pages/home/home';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage: any = HomePage;
+  rootPage:any = HomePage;
 
-  // Test
+// Test
 
-  constructor(
-    platform: Platform,
-    statusBar: StatusBar,
-    splashScreen: SplashScreen
-  ) {
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -25,3 +22,4 @@ export class MyApp {
     });
   }
 }
+
